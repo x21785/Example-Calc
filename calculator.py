072dcfa0132968f0c7b375a6b49e0bfa25d3bc6e
x21785 <91121035+x21785@users.noreply.github.com>
2021-09-23 14:40:53 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
-print('Calcualtor')
+print("Exampale:")
+print('Calculator')
+#defining operations
 def add(x, y):
     return x + y
 def sub(x, y):
@@ -7,24 +9,25 @@ def mul(x, y):
     return x * y
 def div(x, y):
     return x / y
-
-print('1.add')
-print('2.sub')
-print('3.mul')
-print('4.div')
-
-ch = input()
-if ch in('1','2','3','4'):
+while True:
+ print('1.add')
+ print('2.sub')
+ print('3.mul')
+ print('4.div')
+#providing input
+ ch = input()
+ if ch in('1','2','3','4'):
     x = float(input("Num1:"))
     y = float(input("Num2:"))
-
-if ch == '1':
+#printing result
+ if ch == '1':
     print(x,'+',y,'=',add(x, y))
-elif ch == '2':
+ elif ch == '2':
     print(x,'-',y,'=',sub(x, y))
-elif ch == '3':
+ elif ch == '3':
     print(x,'*',y,'=',mul(x, y))
-elif ch == '4':
+ elif ch == '4':
     print(x,'/',y,'=',div(x, y))
-else:
+ else:
     print('Invalid')
+#28 lines of code(excluding comments)
